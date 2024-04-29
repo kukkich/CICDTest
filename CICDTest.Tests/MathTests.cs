17cdccf0c68e7c09f2471bb03a9784cab37eed3c
kukkich <vitia.iezhovqqqq@gmail.com>
2024-04-29 10:44:02 +0700

    Добавил тесты

---
+++ CICDTest.Tests/MathTests.cs
@@ -0,0 +1,38 @@
+﻿namespace CICDTest.Tests;
+
+public class MathTests
+{
+    private int a;
+    private int b;
+
+    [SetUp]
+    public void SetUp()
+    {
+        a = 10;
+        b = 2;
+    }
+
+    [Test]
+    public void SumTest()
+    {
+        var result = a + b;
+
+        Assert.That(result, Is.EqualTo(12));
+    }
+
+    [Test]
+    public void DifferenceTest()
+    {
+        var result = a - b;
+
+        Assert.That(result, Is.EqualTo(8));
+    }
+
+    [Test]
+    public void DivisionTest()
+    {
+        var result = a / b;
+
+        Assert.That(result, Is.EqualTo(5));
+    }
+}
